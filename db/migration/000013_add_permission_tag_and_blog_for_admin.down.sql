628e780faff3403e6613eed450e49e253f013d74
phumiphat <phumiphat.auk@gmail.com>
2024-11-10 18:07:02 +0700

    Add permission tag and blog for admin

---
+++ db/migration/000013_add_permission_tag_and_blog_for_admin.down.sql
@@ -0,0 +1,1 @@
+DELETE FROM role_permission WHERE role_id = 1 AND permission_id IN (6, 7, 8, 9);
---
+++ db/migration/000013_add_permission_tag_and_blog_for_admin.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO role_permission (role_id, permission_id, created_at, updated_at, deleted)
+SELECT 1, 6, now(), now(), false;
+
+INSERT INTO role_permission (role_id, permission_id, created_at, updated_at, deleted)
+SELECT 1, 7, now(), now(), false;
+
+INSERT INTO role_permission (role_id, permission_id, created_at, updated_at, deleted)
+SELECT 1, 8, now(), now(), false;
+
+INSERT INTO role_permission (role_id, permission_id, created_at, updated_at, deleted)
+SELECT 1, 9, now(), now(), false;
